6bf44974d3807047e6c4ac8185bf68855070d860
Ross García Windows <bornsrss14@gmail.com>
2020-07-10 05:09:25 -0500

    Se agrega el cambio del panel de inicio de Sesión ligado a la aplicación desktop

--- src/Main/InicioSesion.java
+++ src/Main/InicioSesion.java
@@ -18,7 +18,6 @@ public class InicioSesion extends javax.swing.JFrame {
      */
     public InicioSesion() {
         initComponents();
-        setBackground(new Color(0,0,0,0));
     }
 
     /**
